3405fc76857a85bb4aa657b018ee78efbe360782
Carl Hester <crucialcarl@gmail.com>
2019-10-28 01:56:36 +0000

    unsure how to manage env variables for aws

--- config/conf_sample.go
+++ config/conf_sample.go
@@ -1,17 +1,20 @@
 package config
 
 //rename this file to conf.go and fix values below
-/*
-const (
-	KEY                   = "MW9S-E7SL-26DU-VV8V" // public use key from bart website
-	STATION               = "MONT"
-	DIR                   = "n"
-	TIMEWIN               = 15
-	AWS_SDK_LOAD_CONFIG   = 1
-	AWS_ACCESS_KEY_ID     = "YYYYYYY"
-	AWS_SECRET_ACCESS_KEY = "XXXXXXX"
-	AWS_DEFAULT_REGION    = "us-east-1"
-	PHONE                 = "+15554443333"
-)
 
+/*
+these environment variables must be set
+
+set AWS_SDK_LOAD_CONFIG=1
+set AWS_ACCESS_KEY_ID=AAAAAA
+set AWS_SECRET_ACCESS_KEY=BBBBBB
+set AWS_DEFAULT_REGION=us-east-1
 */
+
+const (
+	KEY     = "MW9S-E7SL-26DU-VV8V" // public use key from bart website
+	STATION = "MONT"
+	DIR     = "n"
+	TIMEWIN = 15
+	PHONE   = "+14436235265"
+)
