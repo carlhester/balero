19e7401be94abca93712b37c92ddf543b24f5cf4
Carl Hester <crucialcarl@gmail.com>
2019-10-29 04:30:19 +0000

    sort out alerting

--- balero.go
+++ balero.go
@@ -1,5 +1,6 @@
 package main
 
+import "flag"
 import "fmt"
 import "net/http"
 import "io/ioutil"
@@ -13,7 +14,21 @@ import . "balero/sendalerts"
 import . "balero/json2struct"
 
 func main() {
-	url := "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" + config.STATION + "&key=" + config.KEY + "&dir=" + config.DIR + "&json=y"
+	station := config.Station
+	KEY := config.KEY
+	dir := config.Dir
+	targetLine := config.TargetLine
+	timeWindow := config.TimeWindow
+	sendSMS := config.SendSMS
+
+	flag.StringVar(&station, "station", "MONT", "Starting station abbreviation")
+	flag.StringVar(&dir, "dir", "n", "Train direction")
+	flag.StringVar(&targetLine, "line", "YELLOW", "Target Line")
+	flag.StringVar(&sendSMS, "SMS", "N", "Send SMS alerts : Y/N")
+	flag.IntVar(&timeWindow, "time", 15, "Time window threshold for a match")
+	flag.Parse()
+
+	url := "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" + station + "&key=" + KEY + "&dir=" + dir + "&json=y"
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err.Error())
@@ -30,10 +45,8 @@ func main() {
 	var targetMinutes []string
 
 	for _, train := range usableData.Root.Station[0].Etd {
-
 		for _, est := range train.Est {
-
-			if isTargetLine(est.Color) {
+			if strings.EqualFold(est.Color, targetLine) {
 				targetTrains = append(targetTrains, train.Abbreviation)
 				targetMinutes = append(targetMinutes, est.Minutes)
 			}
@@ -46,27 +59,39 @@ func main() {
 	}
 	currTime := time.Now()
 	currTime = currTime.In(loc)
-	fmt.Printf(currTime.Format("Jan _2 15:04:05"))
+	timeStamp := fmt.Sprintf("%s\n", currTime.Format("Jan _2 15:04:05"))
 
+	intMin := convertStrMinutesToInt(targetMinutes)
+	sort.Ints(intMin)
+
+	fmt.Printf("Time: %s", timeStamp)
 	fmt.Printf("\ntargetTrains: %s ", targetTrains)
-	intMinutes := convertStrMinutesToInt(targetMinutes)
-	sort.Ints(intMinutes)
-	fmt.Printf("intMinutes: %d\n", intMinutes)
+	fmt.Printf("\nintMin: %d\n", intMin)
 
-	if len(intMinutes) > 2 {
-		for i, _ := range intMinutes[:len(intMinutes)-2] {
-			twoTrainDelta := intMinutes[i+2] - intMinutes[i]
-			if twoTrainDelta <= config.TIMEWIN {
-				fmt.Printf("Match! %d %d %d : %d\n\n", intMinutes[i], intMinutes[i+1], intMinutes[i+2], twoTrainDelta)
-				alertMsg := fmt.Sprintf("%s %d %d %d : %d", targetTrains, intMinutes[i], intMinutes[i+1], intMinutes[i+2], twoTrainDelta)
-				SendSNS(alertMsg)
+	alertMsg := timeStamp
+	numResults := 0
+
+	if len(intMin) > 2 {
+		for i, _ := range intMin[:len(intMin)-2] {
+			twoTrainDelta := intMin[i+2] - intMin[i]
+			if twoTrainDelta <= timeWindow {
+				fmt.Printf("\nMatch! %d %d %d : %d\n\n", intMin[i], intMin[i+1], intMin[i+2], twoTrainDelta)
+				partAlertMsg := fmt.Sprintf("%s %d %d %d : %d", targetTrains, intMin[i], intMin[i+1], intMin[i+2], twoTrainDelta)
+				alertMsg = fmt.Sprintf("%s\n%s\n", alertMsg, partAlertMsg)
+				numResults += 1
 			}
+		}
+	}
+	if numResults > 0 {
+		fmt.Printf("OUTPUT: %s", alertMsg)
+		if strings.EqualFold(sendSMS, "Y") {
+			SendSNS(alertMsg)
 		}
 	}
 }
 
 func convertStrMinutesToInt(minutes []string) []int {
-	var intMinutes []int
+	var intMin []int
 	for _, strMin := range minutes {
 		if strMin == "Leaving" {
 			strMin = "0"
@@ -75,25 +100,7 @@ func convertStrMinutesToInt(minutes []string) []int {
 		if err != nil {
 			panic(err.Error())
 		}
-		intMinutes = append(intMinutes, i)
+		intMin = append(intMin, i)
 	}
-	return intMinutes
-}
-
-func isTargetStation(station string) bool {
-	switch station {
-	case
-		"ANTC", "CONC", "NCON", "PITT", "PHIL", "WCRK":
-		return true
-	}
-	return false
-}
-
-func isTargetLine(line string) bool {
-	for _, lineColor := range config.TargetStations {
-		if strings.EqualFold(lineColor, line) {
-			return true
-		}
-	}
-	return false
+	return intMin
 }
